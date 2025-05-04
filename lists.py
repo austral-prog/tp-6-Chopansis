abcbc725913b8d2101b720e83ab49c05a57c0066
Francisco Garcia Nirenstein <francisgarcianirenstein@gmail.com>
2025-05-04 20:59:32 -0300

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,20 +1,37 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    Remove = [0,4,5]
+    nueva = list_to_remove_elements[:]
+    for i in sorted(Remove, reverse=True): #esto hace que ordene de mayor a menor y evita que se desordene
+        if i <len(nueva):
+            del nueva[i]
+    return nueva
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    nueva = list_to_add_elements
+    nueva.append("Yellow")
+    nueva.insert(0, "Pink")
+    return nueva 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    nueva = list_to_check
+    if len(nueva) == 0: 
+        return True
+    else: 
+        return False
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
+    if len(list_to_compare1)>= 3 and len(list_to_compare2) >= 3:
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        else: 
+            return False
+    else: 
+        return False
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    nueva = list_of_lists_to_modify
+    nueva[0] = nueva[0][:2]
+    nueva[1] = nueva [1][1:4]
+    nueva[2] = nueva [2][-2:]
+    return nueva
